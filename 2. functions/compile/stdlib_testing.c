2bc883577f1c5ad9b2435fd2b49439c74299dd3d
Bora Un <74696117+UnBora@users.noreply.github.com>
2025-10-29 16:07:24 +0900

    Escape Sequences in C

---
+++ 2. functions/compile/stdlib_testing.c
@@ -0,0 +1,10 @@
+//
+// Created by Un Bora on 10/29/25.
+//
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    printf("Program finished.\n");
+    return EXIT_SUCCESS;  // or EXIT_FAILURE
+}
